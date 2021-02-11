5153cd9b9cca6dbc6492137958d09e511cbef2fd
lude <lude.fal@gmail.com>
2021-02-12 01:17:04 +0900

    Fix problem that index.html cannot display at firebase hosting

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from './firebase/Firebase'
+
+if(process.env.NODE_ENV==='production'){
+  firebase.analytics();
+}
 
 ReactDOM.render(
   <React.StrictMode>
